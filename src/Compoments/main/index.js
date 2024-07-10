3df8752aad2a319bf4b8040fb92b261b079dd02c
muzair-khan <dev.uzairkhan1486@gmail.com>
2024-07-10 11:12:32 -0500

    remove hard coded data and add map to show array

--- src/Compoments/main/index.js
+++ src/Compoments/main/index.js
@@ -1,115 +1,31 @@
 import React from "react";
-import { Box, Button, Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { Container } from "@mui/system";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-function Main({open}) {
+function Main({ open, items, containerRef }) {
   return (
     <Box
       sx={{
-        width: open?"calc(100% - 300px)": "100%",
-        marginLeft: open?"300px":"0px",
+        width: open ? "calc(100% - 300px)" : "100%",
+        marginLeft: open ? "300px" : "0px",
         // md:{width: "calc(100% - 300px)" , marginLeft: "300px",},
         // sm:{width: "100%" ,  marginLeft: "0px"},
 
-       
-      
         height: "100%",
         paddingTop: "80px",
-        transition: "all 1s"
-
+        transition: "all 1s",
       }}
     >
       <Container maxWidth="xl">
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item  xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
-              xs=6 md=8
-            </Box>
-          </Grid>
+        <Grid container spacing={4} ref={containerRef}>
+          {items?.length > 0 &&
+            items?.map((item) => (
+              <Grid item xs={12} sm={6} md={3} key={item}>
+                <Box sx={{ height: "200px", backgroundColor: "#6168e5" }}>
+                  {item}
+                </Box>
+              </Grid>
+            ))}
         </Grid>
       </Container>
     </Box>
