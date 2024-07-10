3768025bf16c0c2d80de3542b789f45cfb1646d1
muzair-khan <dev.uzairkhan1486@gmail.com>
2024-07-10 11:13:10 -0500

    home page for animation

--- src/Screens/home.js
+++ src/Screens/home.js
@@ -1,30 +1,89 @@
-import { Box, Button, Grid } from "@mui/material";
-import { Container } from "@mui/system";
-import React, { useState } from "react";
-import { styled } from "@mui/material/styles";
-
-import Paper from "@mui/material/Paper";
+import { Box } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
 import Sidebar from "../Compoments/sidebar";
 import Header from "../Compoments/header";
 import Main from "../Compoments/main";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
 function Home() {
-  const [sidebar, setSidebar]= useState(true);
+  const [sidebar, setSidebar] = useState(true);
+  const containerRef = useRef(null);
+  const [items, setItems] = useState([1, 2, 3, 4]);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      animateChildren(containerRef.current);
+    }
+  }, []);
+
+  function getFlexItemsInfo(container) {
+    return Array.from(container.children).map((item) => {
+      const rect = item.getBoundingClientRect();
+      return {
+        element: item,
+        x: rect.left,
+        y: rect.top,
+        width: rect.right - rect.left,
+        height: rect.bottom - rect.top,
+      };
+    });
+  }
+  function animateFlexItems(oldFlexItemsInfo, newFlexItemsInfo) {
+    for (const newFlexItemInfo of newFlexItemsInfo) {
+      const oldFlexItemInfo = oldFlexItemsInfo.find(
+        (e) => e.element === newFlexItemInfo.element
+      );
+
+      if (!oldFlexItemInfo) {
+        continue;
+      }
+
+      const translateX = oldFlexItemInfo.x - newFlexItemInfo.x;
+      const translateY = oldFlexItemInfo.y - newFlexItemInfo.y;
+      const scaleX = oldFlexItemInfo.width / newFlexItemInfo.width;
+      const scaleY = oldFlexItemInfo.height / newFlexItemInfo.height;
+
+      newFlexItemInfo.element.animate(
+        [
+          {
+            transform: `translate(${translateX}px, ${translateY}px) scale(${scaleX}, ${scaleY})`,
+          },
+          { transform: "none" },
+        ],
+        {
+          duration: 250,
+          easing: "ease-out",
+        }
+      );
+    }
+  }
+
+  function animateChildren(container) {
+    let oldFlexItemsInfo = getFlexItemsInfo(container);
+
+    const childListMutationCallback = function (mutationsList, observer) {
+      const newFlexItemsInfo = getFlexItemsInfo(container);
+      if (oldFlexItemsInfo) {
+        animateFlexItems(oldFlexItemsInfo, newFlexItemsInfo);
+      }
+      oldFlexItemsInfo = newFlexItemsInfo;
+    };
+
+    new MutationObserver(childListMutationCallback).observe(container, {
+      childList: true,
+    });
+  }
+
+  const handleClick = (e) => {
+    const newItem = items.length + 1;
+    setItems((prevItems) => [newItem, ...prevItems]);
+  };
 
   return (
-    <Box >
-      <Header toggleSidebar={()=>setSidebar(!sidebar)} />
-      <Box sx={{display: "flex"}}>
-        <Sidebar open={sidebar} />
-        <Main open={sidebar}  />
+    <Box>
+      <Header toggleSidebar={() => setSidebar(!sidebar)} />
+      <Box sx={{ display: "flex" }}>
+        <Sidebar open={sidebar} handleClick={handleClick} />
+        <Main open={sidebar} items={items} containerRef={containerRef} />
       </Box>
     </Box>
   );
